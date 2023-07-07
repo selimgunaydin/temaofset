b538bf352a10d0c71996894ce600d83b4e544aae
selimgunaydin <sselimgunaydin@gmail.com>
2023-07-07 20:07:20 +0300

    create

---
+++ src/components/Slider.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Slider() {
+  return (
+    <>
+     <div className="slider">
+     <img src="https://lh3.googleusercontent.com/drive-viewer/AITFw-yrZ4xNHEK541ntRptXpT2d3Ebupe_a4V-tIhV2ZFXbApRVsP4N2qH9muKxNt6dzhq_z7ecrZjUWsITbuan1Vja_0LAvw=s1600" className="w-100" alt="slider" />
+     </div>
+    </>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {} from "@fortawesome/free-solid-svg-icons";
+import { motion } from "framer-motion";
+import Slider from "../components/Slider";
+
+export default function Home() {
+  return (
+    <>
+      <Slider />
+    </>
+  );
+}
